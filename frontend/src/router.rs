409f1b9baf31fe404984f84661dbbac152175d0c
Joseph Lai <jlai24142@gmail.com>
2023-05-27 18:40:20 -0500

    Adding the router component

---
+++ frontend/src/router.rs
@@ -0,0 +1,24 @@
+//! Contains all routes for the site.
+
+use yew_router::prelude::*;
+
+/// Contains all route variants for the site.
+#[derive(Clone, Debug, Eq, PartialEq, Routable)]
+pub enum Route {
+    /// About page.
+    #[at("/about")]
+    About,
+    /// Blog page.
+    #[at("/blog")]
+    Blog,
+    /// 404 page for fuckers tryna dox me or some shit.
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+    /// Single post view.
+    #[at("/blog/post/:post_id")]
+    PostView { post_id: String },
+    /// Root page (landing page).
+    #[at("/")]
+    Root,
+}
