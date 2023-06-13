5700d2fb196974c989206d74cdb378a2f9c2d161
Joseph Lai <jlai24142@gmail.com>
2023-06-12 21:44:50 -0500

    Adding structs for the Story and a Response struct

--- api/src/models/data.rs
+++ api/src/models/data.rs
@@ -8,3 +8,19 @@ pub struct BackgroundGIF {
     /// The Imgur link to the background GIF.
     pub link: String,
 }
+
+/// Contains the story for the 404 page.
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Story {
+    /// The body of the story.
+    pub story: String,
+}
+
+/// Serialize an API response message and status code.
+#[derive(Debug, Serialize)]
+pub struct Response {
+    /// The message associated with this response.
+    pub message: String,
+    /// The status code number associated with this response.
+    pub status_code: u16,
+}
