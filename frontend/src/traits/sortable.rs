b6063f78e01c0d9632a8f691ce546c07c8986b25
Joseph Lai <jlai24142@gmail.com>
2024-01-13 20:03:08 -0600

    Implementing HashMap sorting algorithm

--- frontend/src/traits/sortable.rs
+++ frontend/src/traits/sortable.rs
@@ -1,5 +1,7 @@
 //! Contains the trait and its implementations for sorting `HashMap<String, i32>`s by their values.
 
+use std::collections::HashMap;
+
 use crate::models::chicago::{CleanedShotData, CleanedViolenceData};
 
 /// This trait enables a struct to sort all of its `HashMap` fields in descending order assuming its
@@ -9,25 +11,30 @@ pub trait SortableStruct {
     fn sort_hashmaps(&mut self) {}
 
     /// The sorting algorithm for sorting a single `HashMap<String, i32>` in descending order.
-    fn sort_single_hashmap(&mut self) {}
+    fn sort_single_hashmap(
+        &mut self,
+        unsorted_hashmap: &mut HashMap<String, i32>,
+    ) -> HashMap<String, i32> {
+        let mut sorted_values: Vec<(&String, &i32)> = unsorted_hashmap.iter().collect();
+        sorted_values.sort_by(|first_tuple, second_tuple| second_tuple.1.cmp(first_tuple.1));
+
+        let sorted_hashmap: HashMap<String, i32> = sorted_values
+            .into_iter()
+            .map(|(key, value)| (key.clone(), *value))
+            .collect();
+
+        sorted_hashmap
+    }
 }
 
 impl SortableStruct for CleanedShotData {
     fn sort_hashmaps(&mut self) {
         unimplemented!()
     }
-
-    fn sort_single_hashmap(&mut self) {
-        unimplemented!()
-    }
 }
 
 impl SortableStruct for CleanedViolenceData {
     fn sort_hashmaps(&mut self) {
-        unimplemented!()
-    }
-
-    fn sort_single_hashmap(&mut self) {
         unimplemented!()
     }
 }
