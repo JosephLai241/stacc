49b2f95316efd5c56c4579a718795e74284bd121
Joseph Lai <jlai24142@gmail.com>
2024-01-21 13:50:54 -0600

    Adding a to_vec() method to the trait

--- frontend/src/traits/abstractable_hashmap.rs
+++ frontend/src/traits/abstractable_hashmap.rs
@@ -7,9 +7,14 @@ use crate::{
 
 /// This trait enables a struct to sort all of its `HashMap` fields in descending order assuming its
 /// `HashMap` field is of type `HashMap<String, i32>`.
+/// Additionally, `HashMap`s may be converted into a sorted `Vec<(String, i32)>` representation.
+/// This `Vec` is sorted based on the values in the `HashMap` before conversion.
 pub trait AbstractableHashMap {
     /// Insert a new key/value pair, or increment an existing value of a given key.
     fn insert_or_increment(&mut self, hashmap_name: &str, key: &str) -> Result<(), StaccError>;
+    /// Convert a `HashMap<String, i32>` to a sorted `Vec<(String, i32)>` based on the values in
+    /// the `HashMap`.
+    fn to_vec(&self, hashmap_name: &str) -> Result<Vec<(String, i32)>, StaccError>;
 }
 
 impl AbstractableHashMap for CleanedShotData {
@@ -28,6 +33,27 @@ impl AbstractableHashMap for CleanedShotData {
         *entry += 1;
 
         Ok(())
+    }
+
+    fn to_vec(&self, hashmap_name: &str) -> Result<Vec<(String, i32)>, StaccError> {
+        let hashmap = match hashmap_name {
+            "sorted_blocks" => Ok(&self.sorted_blocks),
+            "sorted_community_areas" => Ok(&self.sorted_community_areas),
+            "sorted_dates" => Ok(&self.sorted_dates),
+            "sorted_incident_types" => Ok(&self.sorted_incident_types),
+            "sorted_rounds" => Ok(&self.sorted_rounds),
+            "sorted_zip_codes" => Ok(&self.sorted_zip_codes),
+            _ => Err(StaccError::InvalidHashMapError(hashmap_name.to_string())),
+        }?;
+
+        let mut sorted_vec: Vec<(String, i32)> = hashmap
+            .iter()
+            .map(|(key, value)| (key.clone(), value.clone()))
+            .collect();
+        sorted_vec.sort_by(|a, b| a.1.cmp(&b.1));
+        sorted_vec.reverse();
+
+        Ok(sorted_vec)
     }
 }
 
@@ -50,5 +76,29 @@ impl AbstractableHashMap for CleanedViolenceData {
         *entry += 1;
 
         Ok(())
+    }
+
+    fn to_vec(&self, hashmap_name: &str) -> Result<Vec<(String, i32)>, StaccError> {
+        let hashmap = match hashmap_name {
+            "sorted_ages" => Ok(&self.sorted_ages),
+            "sorted_community_areas" => Ok(&self.sorted_community_areas),
+            "sorted_dates" => Ok(&self.sorted_dates),
+            "sorted_gun_injury_count" => Ok(&self.sorted_gun_injury_count),
+            "sorted_incident_types" => Ok(&self.sorted_incident_types),
+            "sorted_location_descriptions" => Ok(&self.sorted_location_descriptions),
+            "sorted_victim_races" => Ok(&self.sorted_victim_races),
+            "sorted_victim_sexes" => Ok(&self.sorted_victim_sexes),
+            "sorted_zip_codes" => Ok(&self.sorted_zip_codes),
+            _ => Err(StaccError::InvalidHashMapError(hashmap_name.to_string())),
+        }?;
+
+        let mut sorted_vec: Vec<(String, i32)> = hashmap
+            .iter()
+            .map(|(key, value)| (key.clone(), value.clone()))
+            .collect();
+        sorted_vec.sort_by(|a, b| a.1.cmp(&b.1));
+        sorted_vec.reverse();
+
+        Ok(sorted_vec)
     }
 }
